1193d966a4badeb90487d36b0743f28db7cd2034
Justin Beaulieu <beaulieudev@gmail.com>
2019-02-12 00:02:50 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,6 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    Show.maximum(:rating)
+    Show.maximum("rating")
   end
 
   def self.most_popular_show
@@ -12,12 +12,12 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
-    Show.minimum(:rating)
+    Show.minimum("rating")
   end
 
   def self.ratings_sum
     Show.sum(:rating)
   end
 
-  
+
 end
