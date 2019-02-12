d9b42ebd333df7ab3a3a92a84d0b38e69af476d7
Justin Beaulieu <beaulieudev@gmail.com>
2019-02-12 00:05:33 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,24 +1,23 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    Show.maximum(:rating)
+    Show.maximum("rating")
   end
 
   def self.most_popular_show
-   Show.where(:rating = ?, Show.highest_rating)[0]
+   Show.where("rating = ?", Show.highest_rating)[0]
   end
 
   def self.most_popular_show
-   Show.where(:rating = ?, Show.lowest_rating)[0]
+   Show.where("rating = ?", Show.lowest_rating)[0]
   end
 
   def self.lowest_rating
-    Show.minimum(:rating)
+    Show.minimum("rating")
   end
 
   def self.ratings_sum
-    Show.sum(:rating)
+    Show.sum("rating")
   end
 
-  def
 
 end
